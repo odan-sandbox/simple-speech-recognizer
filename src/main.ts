330844c0a19506ad01132c33d69529c96debd9db
odan <odan3240@gmail.com>
2019-10-08 21:12:29 +0900

    chore(vuetify): add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import Vue from "vue";
+import VueCompositionApi from "@vue/composition-api";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
+Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
